cb5c1bc816130995370ba9faba0bfb15630e4ae5
Denis Pauk <pauk.denis@gmail.com>
2018-03-28 17:20:29 +0300

    Create new fs

--- container/README.md
+++ container/README.md
@@ -14,6 +14,7 @@ make
 ```
 
 Debian/Debootstrap - full operation system:
+Based on https://wiki.debian.org/Debootstrap
 ```shell
 sudo debootstrap --variant=minbase stable stable-chroot http://deb.debian.org/debian/
 ```
@@ -25,6 +26,7 @@ Size with python without update: 254M
 Size bz2: 215M
 
 Alpine - full operation system:
+Based on https://wiki.alpinelinux.org/wiki/Installing_Alpine_Linux_in_a_chroot
 ```shell
 export ALPINE_MIRROR=http://dl-cdn.alpinelinux.org/alpine/
 export ALPINE_VERSION=2.9.1-r0
@@ -34,7 +36,7 @@ mkdir ${ALPINE_ROOT}
 wget ${ALPINE_MIRROR}/latest-stable/main/x86_64/apk-tools-static-${ALPINE_VERSION}.apk
 tar -xzf apk-tools-static-${ALPINE_VERSION}.apk
 sudo ./sbin/apk.static -X ${ALPINE_MIRROR}/latest-stable/main -U --allow-untrusted --root ${ALPINE_ROOT} --initdb add alpine-base
-echo "nameserver 208.67.222.222" | sudo tee ${ALPINE_ROOT}/etc/resolv.conf
+echo -e 'nameserver 208.67.222.222\nnameserver 2620:0:ccc::2' | sudo tee ${ALPINE_ROOT}/etc/resolv.conf
 mkdir -p ${ALPINE_ROOT}/etc/apk
 echo "${ALPINE_MIRROR}${ALPINE_BRANCH}/main" | sudo tee ${ALPINE_ROOT}/etc/apk/repositories
 
@@ -59,3 +61,16 @@ Size with python: 68M
 Size with cloudify: 266M
 
 Ok, lets stop on alpine.
+
+# For run cloudify we need some devices
+
+Like if no urandom: NotImplementedError: /dev/urandom (or equivalent) not found
+```shell
+export ALPINE_ROOT=alpine-root
+sudo mknod -m 666 ${ALPINE_ROOT}/dev/full c 1 7
+sudo mknod -m 666 ${ALPINE_ROOT}/dev/ptmx c 5 2
+sudo mknod -m 644 ${ALPINE_ROOT}/dev/random c 1 8
+sudo mknod -m 644 ${ALPINE_ROOT}/dev/urandom c 1 9
+sudo mknod -m 666 ${ALPINE_ROOT}/dev/zero c 1 5
+sudo mknod -m 666 ${ALPINE_ROOT}/dev/tty c 5 0
+```
--- container/container.go
+++ container/container.go
@@ -7,11 +7,18 @@ import (
 	"syscall"
 )
 
+func makedev (major int, minor int) int {
+  return (minor & 0xff) | (major & 0xfff) << 8
+}
+
 func main() {
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Cloudify error: %s\n", err.Error())
 	}
+
+	// reset umask befor do anything
+	oldUmask := syscall.Umask(0)
 
 	// create and mount all dirs
 	os.Mkdir(path.Join(dir, "sys"),
@@ -42,23 +49,75 @@ func main() {
 	}
 
 	// create and mount all dirs
-	if err := syscall.Mount("sysfs", "/sys", "sysfs", syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_NOSUID, ""); err != nil {
+	if err := syscall.Mount("sysfs", "/sys", "sysfs",
+			syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_NOSUID, ""); err != nil {
 		log.Fatalf("mount sys: %s", err)
 	}
 	defer syscall.Unmount("/sys", syscall.MNT_DETACH)
 
-	if err := syscall.Mount("proc", "/proc", "proc", syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_NOSUID, ""); err != nil {
+	if err := syscall.Mount("proc", "/proc", "proc",
+			syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_NOSUID, ""); err != nil {
 		log.Fatalf("mount proc: %s", err)
 	}
 	defer syscall.Unmount("/proc", syscall.MNT_DETACH)
 
-	if err := syscall.Mount("tmpfs", "/tmp", "tmpfs", 0, "size=65536k"); err != nil {
+	if err := syscall.Mount("tmpfs", "/tmp", "tmpfs",
+			0, "size=65536k,mode=0755"); err != nil {
 		log.Fatalf("mount tmp: %s", err)
 	}
 	defer syscall.Unmount("/tmp", syscall.MNT_DETACH)
 
-	if err := syscall.Mount("tmpfs", "/dev", "tmpfs", 0, "size=65536k"); err != nil {
+	if err := syscall.Mount("tmpfs", "/dev", "tmpfs",
+			0, "size=65536k,mode=0755"); err != nil {
 		log.Fatalf("mount dev: %s", err)
 	}
 	defer syscall.Unmount("/dev", syscall.MNT_DETACH)
+
+	if err := syscall.Mknod("/dev/full",
+			syscall.S_IRUSR | syscall.S_IWUSR |
+			syscall.S_IRGRP | syscall.S_IWGRP |
+			syscall.S_IROTH | syscall.S_IWOTH |
+			syscall.S_IFCHR, makedev(1,  7)); err != nil {
+		log.Fatalf("mknod /dev/full: %s", err)
+	}
+
+	if err := syscall.Mknod("/dev/ptmx",
+			syscall.S_IRUSR | syscall.S_IWUSR |
+			syscall.S_IRGRP | syscall.S_IWGRP |
+			syscall.S_IROTH | syscall.S_IWOTH |
+			syscall.S_IFCHR, makedev(5,  2)); err != nil {
+		log.Fatalf("mknod /dev/ptmx: %s", err)
+	}
+
+	if err := syscall.Mknod("/dev/random",
+			syscall.S_IRUSR | syscall.S_IWUSR |
+			syscall.S_IRGRP | syscall.S_IROTH |
+			syscall.S_IFCHR, makedev(1,  8)); err != nil {
+		log.Fatalf("mknod /dev/random: %s", err)
+	}
+
+	if err := syscall.Mknod("/dev/urandom",
+			syscall.S_IRUSR | syscall.S_IWUSR |
+			syscall.S_IRGRP | syscall.S_IROTH |
+			syscall.S_IFCHR, makedev(1,  9)); err != nil {
+		log.Fatalf("mknod /dev/urandom: %s", err)
+	}
+
+	if err := syscall.Mknod("/dev/zero",
+			syscall.S_IRUSR | syscall.S_IWUSR |
+			syscall.S_IRGRP | syscall.S_IWGRP |
+			syscall.S_IROTH | syscall.S_IWOTH |
+			syscall.S_IFCHR, makedev(1,  5)); err != nil {
+		log.Fatalf("mknod /dev/zero: %s", err)
+	}
+
+	if err := syscall.Mknod("/dev/tty",
+			syscall.S_IRUSR | syscall.S_IWUSR |
+			syscall.S_IRGRP | syscall.S_IWGRP |
+			syscall.S_IROTH | syscall.S_IWOTH |
+			syscall.S_IFCHR, makedev(5,  0)); err != nil {
+		log.Fatalf("mknod /dev/tty: %s", err)
+	}
+	// go back with rights
+	syscall.Umask(oldUmask)
 }
